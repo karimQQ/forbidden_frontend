8f25d430f6dce9183142e501e68ba4813527ad3f
Karim Suleymanov <gchdjjfjfjf@mail.ru>
2024-11-12 23:32:34 +0300

    add tg chanel at home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <p>You can visit my <a href="https://t.me/gdz_forbidden">telegram chanel</a></p>
         </div>
     );
 };
