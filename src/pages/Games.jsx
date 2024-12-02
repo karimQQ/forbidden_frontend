2d0f1d0a2a9050498e10c8a1b54492e821435cf5
Karim Suleymanov <gchdjjfjfjf@mail.ru>
2024-12-02 23:59:10 +0300

    fat dog

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -5,6 +5,7 @@ const Games = () => {
         <div>
             <p><a href="/games/mahjong">Mahjong</a></p>
             <p><a href="/games/2048">2048</a></p>
+            <p><a href="/games/dog/game.html">Fat dog</a></p>
         </div>
     );
 };
