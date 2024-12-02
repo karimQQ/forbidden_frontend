0b95d305f2b58790a034910d5c8ecb0fb49ff72e
Karim Suleymanov <gchdjjfjfjf@mail.ru>
2024-12-02 22:25:17 +0300

    2048

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -4,6 +4,7 @@ const Games = () => {
     return (
         <div>
             <p><a href="/games/mahjong">Mahjong</a></p>
+            <p><a href="/games/2048">2048</a></p>
         </div>
     );
 };
