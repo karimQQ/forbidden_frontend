291b0168b5ac51d1404e1685cf51064af2554aab
Karim Suleymanov <gchdjjfjfjf@mail.ru>
2024-12-03 22:39:48 +0300

    Update Games.jsx

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -4,7 +4,7 @@ const Games = () => {
     return (
         <div>
             <p><a href="/games/mahjong">Mahjong</a></p>
-            <p><a href="/games/2048">2048</a></p>
+            <p><a href="/games/2048/">2048</a></p>
             <p><a href="/games/dog/game.html">Fat dog</a></p>
         </div>
     );
