5a42c9af92e10783986a32092c023a2240c86354
karimQQ <95043700+karimQQ@users.noreply.github.com>
2025-04-23 15:00:19 +0300

    aga

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -6,7 +6,7 @@ const Games = () => {
         <div>
             <Navbar/>
             <main>
-                <p><a href="http://forbiddengdz.ru:8777/games/web/index.html">Online game</a></p>
+                <p><a href="http://forbiddengdz.ru:8777/games/web/index.html">Fat dog ONLINE</a></p>
                 <p><a href="/games/mahjong/">Mahjong</a></p>
                 <p><a href="/games/2048/">2048</a></p>
                 <p><a href="/games/dog/game.html">Fat dog</a></p>
