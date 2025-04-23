a15c9780577270b2b229ac51c506b5c2312220ce
karimQQ <95043700+karimQQ@users.noreply.github.com>
2025-04-23 14:47:31 +0300

    Update Games.jsx

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -6,10 +6,10 @@ const Games = () => {
         <div>
             <Navbar/>
             <main>
+                <p><a href="http://forbiddengdz.ru:8777/games/web/index.html">Online game</a></p>
                 <p><a href="/games/mahjong/">Mahjong</a></p>
                 <p><a href="/games/2048/">2048</a></p>
                 <p><a href="/games/dog/game.html">Fat dog</a></p>
-                <p><a href="http://forbiddengdz.ru:8777/games/online_game/index.html">Online game</a></p>
             </main>
         </div>
     );
